14ca9224d960ba993c9af65e0a77e778940eded9
VijethaBC <123502391+VijethaBC@users.noreply.github.com>
2023-12-07 00:39:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('addToCartBtn').addEventListener('click', function() {
+    alert('Product added to cart!');
+  });
+  
+
+  
+document.getElementById('addToCartBtn').addEventListener('click', function() {
+  const itemPrice = 300.00 ; 
+  alert('Item added to cart! Cost: ' + itemPrice + 'Rs');
+});
